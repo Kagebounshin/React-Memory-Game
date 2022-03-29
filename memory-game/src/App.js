59a9c22430436c2f2a163256556549bc30e2b509
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2022-03-29 17:26:30 +0000

    worked on handleClick function, styled the cards and header

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css';
+import SingleCard from './components/SingleCard';
 
 const cardImages = [
   { "src": "/img/animal.png"},
@@ -13,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null) 
+  const [choiceTwo, setChoiceTwo] = useState(null) 
 
   // Shuffle Cards
   const shuffleCards = () => {
@@ -24,13 +27,25 @@ function App() {
       setTurns(0)
   }
 
-  console.log(cards, turns)
-
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
 
   return (
     <div className="App">
-      <h1>Memory Game</h1> 
+      <h1 className='header'>Animals</h1> 
       <button onClick={shuffleCards}>New Game</button>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SingleCard 
+            key={card.id} 
+            card={card} 
+            handleChoice={handleChoice}
+          />
+        ))}
+      </div>
     </div>
   );
 }
