91f7c8f1a56395f551a485e96699595ffb4ee441
Kagebounshin <kristoffer-heiskanen@hotmail.com>
2022-03-29 16:39:03 +0000

    Added a shuffle card function

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,10 +1,36 @@
+import { useState } from 'react'
 import './App.css';
 
+const cardImages = [
+  { "src": "/img/animal.png"},
+  { "src": "/img/animal01.png"},
+  { "src": "/img/animal02.png"},
+  { "src": "/img/animal03.png"},
+  { "src": "/img/animal04.png"},
+  { "src": "/img/zebra.png"}
+]
+
 function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  // Shuffle Cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }))
+
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
+  console.log(cards, turns)
+
+
   return (
     <div className="App">
-      <h1>Memory Game</h1>
-      <button>New Game</button>
+      <h1>Memory Game</h1> 
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
